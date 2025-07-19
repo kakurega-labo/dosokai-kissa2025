d9cd31d826545c8a707c3eaf9116708b65853936
kakurega-labo <udon05.0025@gmail.com>
2025-07-19 13:28:05 +0900

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+//js/firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzl6UB-bN7BI58EZc1Mm2eTTYRDKWEnNo",
+  authDomain: "dosokai-kissa.firebaseapp.com",
+  projectId: "dosokai-kissa",
+  storageBucket: "dosokai-kissa.firebasestorage.app",
+  messagingSenderId: "165188890725",
+  appId: "1:165188890725:web:b39e15c9798dc9fcef168c"
+};
+
+// Firebase初期化
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
